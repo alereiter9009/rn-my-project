93ac117bfeaa16b8eda5f5cb1b344611b6044cb2
alereiter9009 <55386668+alereiter9009@users.noreply.github.com>
2020-04-22 11:36:56 +0100

    Project Report and Documentation added
    As you've asked, i added the files on a folder named Project Docs.
    
    Please notice i am currently going through the About page and trying to figure out how will I design it and what am i going to display there.
    
    I also organised the style objects on a few screens to make the code more organised.

--- src/components/ImagesLayoutComponent.js
+++ src/components/ImagesLayoutComponent.js
@@ -5,28 +5,18 @@ const ImagesLayoutComponent = ({ imageSource, title, urlLink }) => {
 
 
   return (
-    <View style={{ flex: 1, flexDirection: 'row', borderBottomWidth: 2, }}>
-      <View style={{ flex: 1, flexDirection: 'column' }}>
+    <View style={styles.container}>
+      <View style={styles.imageDishesContainer}>
         <Image
-          style={{
-            flex: 1,
-            height: null,
-            resizeMode: 'stretch',
-            width: null,
-          }}
+          style={styles.imageDishesStyles}
           source={imageSource}
         />
       </View>
-      <View style={{ flex: 1, flexDirection: 'column', borderLeftWidth: 1, borderLeftColor: '#ff8cb1' }}>
-        <Text style={{
-          fontSize: 20,
-          fontWeight: 'bold',
-          color: '#67cdf5',
-          textAlign: 'center',
-        }}>{title}</Text>
+      <View style={styles.imageDescriptionContainer}>
+        <Text style={styles.imageDescriptionText}>{title}</Text>
         <TouchableOpacity>
-        <View style={{backgroundColor: '#ffc382', marginRight: 30, marginLeft: 30, marginTop: 10, marginBottom: 5}}>
-          <Text style ={{textAlign: 'center', fontWeight: 'bold'}}onPress={urlLink}>Open on Google</Text>
+        <View style={styles.googleButtonContainer}>
+          <Text style ={styles.googleTextStyle}onPress={urlLink}>Open on Google</Text>
         </View>
         </TouchableOpacity>
       </View>
@@ -34,6 +24,48 @@ const ImagesLayoutComponent = ({ imageSource, title, urlLink }) => {
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1, 
+    flexDirection: 'row', 
+    borderBottomWidth: 2,
+  },
+  imageDishesContainer: {
+    flex: 1, 
+    flexDirection: 'column' 
+  },
+  imageDishesStyles: {
+    flex: 1,
+    height: null,
+    resizeMode: 'stretch',
+    width: null,
+  },
+  imageDescriptionContainer: {
+    flex: 1, 
+    flexDirection: 'column',
+    borderLeftWidth: 1, 
+    borderLeftColor: '#ff8cb1',
+  },
+  imageDescriptionText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: '#67cdf5',
+    textAlign: 'center',
+  }, 
+  googleButtonContainer: {
+    backgroundColor: '#ffc382', 
+    marginRight: 30, 
+    marginLeft: 30, 
+    marginTop: 10, 
+    marginBottom: 5, 
+    borderWidth: 2, 
+    borderRadius: 20, 
+    borderColor: '#ffc382', 
+  }, 
+  googleTextStyle: {
+    textAlign: 'center', 
+    fontWeight: 'bold', 
+  }, 
+});
 
 export default ImagesLayoutComponent;
--- src/screens/AboutScreen.js
+++ src/screens/AboutScreen.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import { Text, StyleSheet, View, Image, ScrollView} from 'react-native';
-import ImageHeaderComponent from '../components/ImageHeaderComponent';
+import { Text, StyleSheet, View, Image, TouchableOpacity, Linking } from 'react-native';
 
 const AboutScreen = () => {
 
+  const contactLink = () => { Linking.openURL('https://www.linkedin.com/in/alexander-reiter');};
   return (
     <View style={{ flex: 1 }}>
-      <View style={{ flex: 0.65 }}>
-        <ImageHeaderComponent
-          imageSource={require('../../assets/aboutus.png')}
-          header="About" />
+      <View style={{ flexDirection: 'row', backgroundColor: '#67cdf5', height: 115 }}>
       </View>
-      <ScrollView style={{flex: 1, flexDirection: 'column'}} > 
+      <View
+        style={{ position: 'absolute', top: 35, left: 100, right: 0, bottom: 0 }}>
+        <Image
+          style={{ height: 150, width: 150, }}
+          source={require('../../assets/alexander.png')}
+        />
+      </View>
+      <View>
+      <TouchableOpacity>
+        <View style={styles.contactButtonContainer}>
+          <Text style ={styles.contactTextStyle}onPress={contactLink}>CONTACT</Text>
+        </View>
+        </TouchableOpacity>
+      </View>
 
-      </ScrollView>
-      
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+
+  contactButtonContainer: {
+    backgroundColor: '#67cdf5',
+    borderWidth: 2,
+    borderRadius: 5,
+    marginRight: 120,
+    marginLeft: 120,
+    borderColor: '#67cdf5',
+    marginTop: 90,
+  },
+  contactTextStyle: {
+    textAlign: 'center',
+    fontWeight: 'bold',
+  },
+
+});
 
 export default AboutScreen;
